5636eccab784f026c540336626110d52b784735f
KEFICO\22011083 <SungHyun.Park@hyundai-kefico.com>
2023-12-07 10:44:05 +0900

    no message

--- Test_Initial.m
+++ Test_Initial.m
@@ -1,4 +1,4 @@
 % Cfg Set
 addpath("C:\Users\Administrator\MATLAB\Projects\Simulink_Test");
 load_system('C:\Users\Administrator\MATLAB\Projects\Simulink_Test\Example.slx'); 
-sltest.testmanager.load('C:\Matlab_Example\Simulink_TestManager.mldatx');
+sltest.testmanager.load('C:\Users\Administrator\MATLAB\Projects\Simulink_Test\Simulink_TestManager.mldatx');
